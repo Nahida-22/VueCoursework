9debf715f37f99a488f62986ea4f1efa829436d6
Nahida Rojoa <nr681@live.mdx.ac.uk>
2025-11-08 17:53:15 +0400

    Implemented courses.js file

---
+++ courses.js
@@ -0,0 +1,50 @@
+
+
+let courses = [
+    {
+            id:"0001",
+            title: 'Mathematics 101',
+            description: 'An introductory course to Mathematics.',
+            image: 'assets/MathCourse.png',
+            location: 'Building A, Room 101',
+            price: 100,
+            availableInventory: 5
+          },
+          {
+            id:"0002",
+            title: 'Science 301',
+            description: 'Exploring the wonders of Science.',
+            image: 'assets/scienceCourse.jpg',
+            location: 'Building B, Room 202',
+            price: 150,
+            availableInventory: 3
+          },
+          {
+            id:"0003",
+            title: 'History 201',
+            description: 'A journey through historical events.',
+            image: 'assets/historyCourse.jpg',
+            location: 'Building C, Room 303',
+            price: 120,
+            availableInventory: 4
+          },
+          {
+            id:"0004",
+            title: 'Art 101',
+            description: 'Discovering the world of Art.',
+            image: 'assets/artCourse.jpg',
+            location: 'Building D, Room 404',
+            price: 130,
+            availableInventory: 2
+          },
+          {
+            id:"0005",
+            title: 'Computer Science 101',
+            description: 'Introduction to Computer Science concepts.',
+            image: 'assets/computerScienceCourse.jpg',
+            location: 'Building E, Room 505',
+            price: 200,
+            availableInventory: 6
+          }
+
+]
