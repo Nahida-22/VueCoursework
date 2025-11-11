e9dee325b4f53638b6a691cd97cc1e98969aa795
Nahida Rojoa <nr681@live.mdx.ac.uk>
2025-11-11 18:02:54 +0400

    Folder structure for front-end

---
+++ courses.js
@@ -0,0 +1,92 @@
+let courses=[
+  {
+    "id": "0001",
+    "title": "Mathematics 101",
+    "description": "An introductory course to Mathematics.",
+    "image": "assets/MathCourse.png",
+    "location": "Building A, Room 101",
+    "price": 100,
+    "availableInventory": 5
+  },
+  {
+    "id": "0002",
+    "title": "Science 301",
+    "description": "Exploring the wonders of Science.",
+    "image": "assets/scienceCourse.jpg",
+    "location": "Building B, Room 202",
+    "price": 150,
+    "availableInventory": 3
+  },
+  {
+    "id": "0003",
+    "title": "History 201",
+    "description": "A journey through historical events.",
+    "image": "assets/historyCourse.jpg",
+    "location": "Building C, Room 303",
+    "price": 120,
+    "availableInventory": 4
+  },
+  {
+    "id": "0004",
+    "title": "Art 101",
+    "description": "Discovering the world of Art.",
+    "image": "assets/artCourse.jpg",
+    "location": "Building D, Room 404",
+    "price": 130,
+    "availableInventory": 2
+  },
+  {
+    "id": "0005",
+    "title": "Computer Science 101",
+    "description": "Introduction to Computer Science concepts.",
+    "image": "assets/computerScienceCourse.jpg",
+    "location": "Building E, Room 505",
+    "price": 200,
+    "availableInventory": 6
+  },
+  {
+    "id": "0006",
+    "title": "French Beginner class",
+    "image": "assets/frenchCourse.jpeg",
+    "description": "The ultimate french learning course for beginners",
+    "location": "Building D, Room 403",
+    "price": 250,
+    "availableInventory": 15
+  },
+  {
+    "id": "0007",
+    "title": "ACCA Level 1",
+    "image": "assets/ACCA.jpg",
+    "description": "The ultimate crash course",
+    "location": "Building A, Room 103",
+    "price": 300,
+    "availableInventory": 20
+  },
+  {
+    "id": "0008",
+    "title": "Economics and Finance",
+    "image": "assets/economicsCourse.jpeg",
+    "description": "A comprehensive course",
+    "location": "Building A, Room 105",
+    "price": 500,
+    "availableInventory": 10
+  },
+  {
+    "id": "0009",
+    "title": "Law and Finance",
+    "image": "assets/financeLawCourse.jpg",
+    "description": "Learn more about the world of finance in Law",
+    "location": "Building B, Room 205",
+    "price": 400,
+    "availableInventory": 13
+  },
+  {
+    "id": "0010",
+    "title": "Business Studies",
+    "image": "assets/businessCourse.png",
+    "description": "An introduction to Business Management",
+    "location": "Building c, Room 306",
+    "price": 110,
+    "availableInventory": 20
+  }
+]
